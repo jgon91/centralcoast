fd7281ce716101094b24d8d87619c946d13c775e
leoneljunior <LeonelJR@MacBook-Pro-de-Raimundo.local>
2015-06-22 10:54:14 -0700

    no message

--- heavyconnect/static/js/login.js
+++ heavyconnect/static/js/login.js
@@ -7,6 +7,42 @@ function process_login() {
 		success: function(data, status, xhr){
 			if(data.success){
 				location.reload();
+			}else if(data == 1){
+				$("#errorMessage").html("");
+			}
+		}
+	});
+}
+
+function getUserInformation(token) {
+	console.log("function called")
+	$.ajax({
+		method: "POST",
+		url: "login/",
+		data: 'csrf_token':token ,
+		success: function(data, status, xhr){
+			$("#lastName").html(data.lastName);
+		}
+	});
+	
+	$.ajax({
+		method: "POST",
+		url: "login/",
+		data: 'csrf_token':token ,
+		success: function(data, status, xhr){
+			if(data.success){
+				location.reload();
+			}
+		}
+	});
+	
+	$.ajax({
+		method: "POST",
+		url: "login/",
+		data: 'csrf_token':token ,
+		success: function(data, status, xhr){
+			if(data.success){
+				location.reload();
 			}
 		}
 	});
