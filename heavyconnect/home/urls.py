e2009ee1bca84f4634bd5b085da1f5c06300cac0
André Menezes <menezescode@gmail.com>
2015-06-23 16:20:59 -0700

    Changed the urls.py file to be more organized. Started working on issue #21 that could not be completed.

--- heavyconnect/home/urls.py
+++ heavyconnect/home/urls.py
@@ -6,8 +6,15 @@ urlpatterns = patterns('',
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^driver/$', driver, name='driver'),
+    
+    ### Functions ###
     url(r'^employeeLocation/$', getEmployeeLocation, name = 'employeeLocation'),
+    url(r'^driverInfo/$', getDriverInformation, name = 'driverInfo'),
+    url(r'^startNewTask/$', startNewTask, name = 'startNewTask'),
+    ### End ###
+
+    ### Froms ###
     url(r'^formTEST/$', manufacturerForm, name='registerManufacturer'),
     url(r'^formok/$', formok, name = 'formok'),
-    url(r'^driverInfo/$', getDriverInformation, name = 'driverInfo'),
+    ### End ###
 )
--- heavyconnect/home/views.py
+++ heavyconnect/home/views.py
@@ -21,6 +21,26 @@ def driver(request):
 	return render(request, 'driver.html')
 
 @login_required
+def startNewTask(request):
+	result = {'success' : False}
+
+	if request.method == 'POST':
+		if request.is_ajax():
+			try:
+				employee = Employee.objects.get(user_id = request.user.id)
+				# if employee.permission_level = 2:
+				# 	render start task page
+				# else:
+				# 	user does not have permission
+			except DoesNotExist:
+				result['code'] ==  1 #There is no users associated with this
+		else:
+			result['code'] == 4 #Use ajax to perform requests
+	else:
+		result['code'] == 5 #Request was not POST
+	return HttpResponse(json.dumps(result),content_type='application/json')
+
+@login_required
 def getEmployeeLocation(request):
 	result = {'success' : False}
 
@@ -133,12 +153,11 @@ Just a quick explanation on how to test forms:
 	    return render(request, 'THEPAGEYOUWANTTOBERETURNED.html', {
 	        'form': form,
 	    })
-If you need to test the forms change the form name.
 </menezescode>
 
 @menezescode: 	Those are the forms. At the current point 06/22/2015,
 				each view does nothgin besides render the page and redirect
-				to a different page if it's correct and reload the page 
+				to a different page (formok) if it's correct and reload the page 
 				if the form was sent incorrectly
 
 
