aa94f33f9e1872673a1d8dbebb37731dd01f0575
Tarek Holanda <tarekdasilva@hotmail.com>
2015-07-19 22:57:42 -0700

    Taskflow page - New Layout Done.

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -80,15 +80,29 @@ WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#       'NAME': 'd1s6n1u3qgoqrc',                      # Or path to database file if using sqlite3.
+#       'USER': 'ezlbhdoafnhwpe',                      # Not used with sqlite3.
+#       'PASSWORD': 'WFBvlJ2rgCcZoPwIGRZsmS77S2',                  # Not used with sqlite3.
+#       'HOST': 'ec2-54-197-238-19.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+#       'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+#   }
+#}
+
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-       'NAME': 'd1s6n1u3qgoqrc',                      # Or path to database file if using sqlite3.
-       'USER': 'ezlbhdoafnhwpe',                      # Not used with sqlite3.
-       'PASSWORD': 'WFBvlJ2rgCcZoPwIGRZsmS77S2',                  # Not used with sqlite3.
-       'HOST': 'ec2-54-197-238-19.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
-       'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dev',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
+
+    }
 }
 
 
