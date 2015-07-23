b6367f917b9f555b3960f6876f78ad2522e00ad5
Bruno Soares da Silva <bdasilva@csumb.edu>
2015-07-23 16:09:06 -0700

    --Adding database settings for demo app

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -93,18 +93,15 @@ WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dev',                      # Or path to database file if using sqlite3.
-        'USER': 'LeonelJR',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'd9gfceh6642m3k',                      # Or path to database file if using sqlite3.
+        'USER': 'oiyfrcynbvlyev',                      # Not used with sqlite3.
+        'PASSWORD': 'QI9jiCzj-h6WnhlSEiooEV9pnU',                  # Not used with sqlite3.
+        'HOST': 'ec2-54-83-41-183.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
 
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
