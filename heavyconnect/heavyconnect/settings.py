47309fdda4deadcb9eaa0ee58ae09c0fcade02d8
Jessica Gonzalez <jgon91@gmail.com>
2015-07-30 00:28:55 -0700

    adding database credentials

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
-        'USER': 'andremenezes',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'd7gq7qhrof4f76',                      # Or path to database file if using sqlite3.
+        'USER': 'izwubhoscwicdw',                      # Not used with sqlite3.
+        'PASSWORD': 'k4eohceFS4vJ0-AZibSHVvLyeB',                  # Not used with sqlite3.
+        'HOST': 'ec2-107-20-220-251.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
