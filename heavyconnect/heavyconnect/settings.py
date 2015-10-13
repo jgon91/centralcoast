86bf59b5aba87b7630a74123e023208dd5eb34ba
Jessica Gonzalez <jgon91@gmail.com>
2015-10-13 06:08:21 -0700

    changing db for t-and-a

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -96,28 +96,28 @@ if 'RDS_DB_NAME' in os.environ:
     }
 
 else:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-    #         'NAME': 'd9v864930h13f8',                      # Or path to database file if using sqlite3.
-    #         'USER': 'hilrmvqeqgzmml',                      # Not used with sqlite3.
-    #         'PASSWORD': 'cYqCAPTj4pEaPItbupZv2JvhYp',                  # Not used with sqlite3.
-    #         'HOST': 'ec2-54-83-10-210.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
-    #         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-    #
-    #     }
-    # }
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'dev',                      # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+            'NAME': 'd9v864930h13f8',                      # Or path to database file if using sqlite3.
+            'USER': 'hilrmvqeqgzmml',                      # Not used with sqlite3.
+            'PASSWORD': 'cYqCAPTj4pEaPItbupZv2JvhYp',                  # Not used with sqlite3.
+            'HOST': 'ec2-54-83-10-210.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
 
         }
     }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #         'NAME': 'dev',                      # Or path to database file if using sqlite3.
+    #         'USER': '',                      # Not used with sqlite3.
+    #         'PASSWORD': '',                  # Not used with sqlite3.
+    #         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+    #         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+    #
+    #     }
+    # }
 
 
 
