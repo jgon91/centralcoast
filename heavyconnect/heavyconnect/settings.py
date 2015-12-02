0a2ceb8826df8b80b7fbee6a33d9a964366f02c7
Jessica Gonzalez <jgon91@gmail.com>
2015-12-02 11:12:08 -0800

    scarni db cred

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-       'NAME': 'd88qocjdhrfs02',                      # Or path to database file if using sqlite3.
-       'USER': 'uirthkgwudvbbp',                      # Not used with sqlite3.
-       'PASSWORD': 'NRTNSXmx6y5pCkwnS6xlXRuvLP',                  # Not used with sqlite3.
-       'HOST': 'ec2-107-21-219-235.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+       'NAME': 'd5j0r9l1p6hbs1',                      # Or path to database file if using sqlite3.
+       'USER': 'lvmlaczuulaeir',                      # Not used with sqlite3.
+       'PASSWORD': 'uLbuWqcYB2GCNFAZMatjN9uTip',                  # Not used with sqlite3.
+       'HOST': 'ec2-107-21-219-109.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
       'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
 
    }
