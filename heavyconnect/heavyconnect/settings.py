66b0049997fae3012aecc7a2116aabdfff9e248b
Jessica Gonzalez <jgon91@gmail.com>
2016-01-04 13:50:22 -0800

    fixing error messages

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -44,12 +44,12 @@ INSTALLED_APPS = (
     'home',
 )
 
-ADMINS = [('Jessica', 'jessica@heavyconnect.com')]
+ADMINS = [('Jessica', 'jgon91@gmail.com')]
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jessica@heavyconnect.com'
-EMAIL_HOST_PASSWORD = 'Bender20!'
-SERVER_EMAIL = 'jessica@heavyconnect.com'
+EMAIL_HOST_USER = 'jgon91@gmail.com'
+EMAIL_HOST_PASSWORD = 'AngelaMunoz91!'
+SERVER_EMAIL = 'jgon91@gmail.com'
 MANAGERS = ADMINS
 EMAIL_USE_TLS = True
 
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 DATABASES = {
          'default': {
              'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-             'NAME': 'd395pa1l0s6i0g',                      # Or path to database file if using sqlite3.
-             'USER': 'dntkrypwpilsae',                      # Not used with sqlite3.
-             'PASSWORD': 'QAUjydqmetOvNSew_L1c309zd6',                  # Not used with sqlite3.
-             'HOST': 'ec2-54-204-12-25.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+             'NAME': 'd3ghphbaaf2alv',                      # Or path to database file if using sqlite3.
+             'USER': 'lhiebmankdbyyt',                      # Not used with sqlite3.
+             'PASSWORD': 'Anko1tlUH6zuxLbAhsSlPmuLP1',                  # Not used with sqlite3.
+             'HOST': 'ec2-54-197-247-170.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
              'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
 
          }
