86f72115b78c4114e3e9cb2a0567721881a73b3b
Jessica Gonzalez <jgon91@gmail.com>
2016-01-10 14:22:14 -0800

    adding migrations

---
+++ heavyconnect/home/migrations/0046_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0045_client'),
+        ('home', '0015_auto_20151013_1109'),
+    ]
+
+    operations = [
+    ]
