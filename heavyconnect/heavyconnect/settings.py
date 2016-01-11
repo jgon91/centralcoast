d9bcc6e79720abcb85d6400ac192a50bdaf84628
Jessica Gonzalez <jgon91@gmail.com>
2016-01-11 12:04:34 -0800

    fixing db settings

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -108,17 +108,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 
 
-DATABASES = {
-         'default': {
-             'ENGINE': 'tenant_schemas.postgresql_backend', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-             'NAME': 'dev',                      # Or path to database file if using sqlite3.
-             'USER': '',                      # Not used with sqlite3.
-             'PASSWORD': '',                  # Not used with sqlite3.
-             'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-             'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-
-         }
-     }
+# DATABASES = {
+#          'default': {
+#              'ENGINE': 'tenant_schemas.postgresql_backend', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#              'NAME': 'dev',                      # Or path to database file if using sqlite3.
+#              'USER': '',                      # Not used with sqlite3.
+#              'PASSWORD': '',                  # Not used with sqlite3.
+#              'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#              'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+#
+#          }
+#      }
 #demo
 DATABASES = {
     'default': {
