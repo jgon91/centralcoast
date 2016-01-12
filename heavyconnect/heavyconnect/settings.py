2432e4e538dfa38a55b11cdf51b98253b4ac7df5
Jessica Gonzalez <jgon91@gmail.com>
2016-01-11 17:34:54 -0800

    tanda db

--- heavyconnect/heavyconnect/settings.py
+++ heavyconnect/heavyconnect/settings.py
@@ -107,7 +107,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 
-
+#
 # DATABASES = {
 #          'default': {
 #              'ENGINE': 'tenant_schemas.postgresql_backend', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -120,13 +120,25 @@ WSGI_APPLICATION = 'heavyconnect.wsgi.application'
 #          }
 #      }
 #demo
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'tenant_schemas.postgresql_backend',
+#         'NAME': 'd3ghphbaaf2alv',                      # Or path to database file if using sqlite3.
+#         'USER': 'lhiebmankdbyyt',                      # Not used with sqlite3.
+#         'PASSWORD': 'Anko1tlUH6zuxLbAhsSlPmuLP1',                  # Not used with sqlite3.
+#         'HOST': 'ec2-54-197-247-170.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '5432',
+#     }
+# }
+
+#tanda
 DATABASES = {
     'default': {
         'ENGINE': 'tenant_schemas.postgresql_backend',
-        'NAME': 'd3ghphbaaf2alv',                      # Or path to database file if using sqlite3.
-        'USER': 'lhiebmankdbyyt',                      # Not used with sqlite3.
-        'PASSWORD': 'Anko1tlUH6zuxLbAhsSlPmuLP1',                  # Not used with sqlite3.
-        'HOST': 'ec2-54-197-247-170.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'd395pa1l0s6i0g',                      # Or path to database file if using sqlite3.
+        'USER': 'dntkrypwpilsae',                      # Not used with sqlite3.
+        'PASSWORD': 'QAUjydqmetOvNSew_L1c309zd6',                  # Not used with sqlite3.
+        'HOST': 'ec2-54-204-12-25.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',
     }
 }
--- heavyconnect/home/views.py
+++ heavyconnect/home/views.py
@@ -39,12 +39,12 @@ LANGUAGE_CHOICES = ['pt-br','es', 'en-us']
 #                 on_trial=False)
 # tenant.save()
 #
-# tenant = Client(domain_url='test.heavyconnect.com', # don't add your port or www here!
-#                 schema_name='test1',
-#                 name='Fonzy Tenant',
-#                 paid_until='2014-12-05',
-#                 on_trial=True)
-# tenant.save() # migrate_schemas automatically called, your tenant is ready to be used!
+tenant = Client(domain_url='t-and-a.heavyconnect.com', # don't add your port or www here!
+                schema_name='tanda',
+                name='T & A',
+                paid_until='2014-12-05',
+                on_trial=True)
+tenant.save() # migrate_schemas automatically called, your tenant is ready to be used!
 
 class HelloPDFView(PDFTemplateView):
 	template_name = "template/timecard.html"
